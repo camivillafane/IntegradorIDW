f09c6a334f12df7db4fd0fe1635d9e96cd2ff132
lujiiroud <lujanroude039@gmail.com>
2025-06-01 22:06:29 -0300

    editar salon e ID

--- accionesSalon.js
+++ accionesSalon.js
@@ -1,22 +1,17 @@
-function editarSalon(index){
-    const salones = JSON.parse(localStorage.getItem('salones')) || []; //obtengo cadena de salones para convertirla a objetos, si no tengo nada que cree no vacio
-    const salon = salones[index]; //del array de salones me quedo con uno que en este caso es index, es decir la posicion que le pasamos del array
-    
-    document.getElementById('nombreSalon').value = salon.nombreSalon;//necesitamos que renderice los valores que obtenemos en el formulario
+window.editarSalon = function (index) {
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];
+    const salon = salones[index];
+
+    document.getElementById('nombreSalon').value = salon.nombreSalon;
     document.getElementById('direccionSalon').value = salon.direccionSalon;
     document.getElementById('descripcionSalon').value = salon.descripcionSalon;
 
-    salones.splice(index,1);//no se puede hacer push, pq yo no quiero sumar longitud a i array, lo que se busca es remplazar, l push va a agregar valor
-    localStorage.setItem('salones',JSON.stringify(salones));
-    mostrarSalones();                                       
+    indexEnEdicion = index; 
 }
 
-function eliminarSalon(index){
-    const salones = JSON.parse(localStorage.getItem('salones')) || []; //obtengo cadena de salones para convertirla a objetos, si no tengo nada que cree no vacio
-    const salon = salones[index]; //del array de salones me quedo con uno que en este caso es index, es decir la posicion que le pasamos del array
-    
-
-    salones.splice(index,1);//no se puede hacer push, pq yo no quiero sumar longitud a i array, lo que se busca es remplazar, l push va a agregar valor
-    localStorage.setItem('salones',JSON.stringify(salones));
-    mostrarSalones();   
+window.eliminarSalon = function (index) {
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];
+    salones.splice(index, 1);
+    localStorage.setItem('salones', JSON.stringify(salones));
+    mostrarSalones();
 }
